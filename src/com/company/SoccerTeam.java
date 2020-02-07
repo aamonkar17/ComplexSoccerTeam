a372ed3e257c6e0772695189fa32d2b72c6ef140
Ankit Amonkar <ankit.amonkar@gmail.com>
2020-02-07 12:28:09 -0500

    initial

--- src/com/company/SoccerTeam.java
+++ src/com/company/SoccerTeam.java
@@ -1,5 +1,34 @@
 package com.company;
 
 public class SoccerTeam {
-    
+    int wins;
+    int loss;
+    int tie;
+    public SoccerTeam(w, l, t){
+        wins = w;
+        loss = l;
+        tie = t;
+    }
+    public void played(SoccerTeam other,  int  myScore, int otherScore){
+        if(myScore>otherScore){
+            this.wins++;
+            other.loss++;
+        }
+        else if(myScore == otherScore){
+            this.tie++;
+            other.tie++;
+        }
+        else{
+            this.loss++;
+            other.wins++;
+            }
+    }
+    public int numberPts(SoccerTeam a){
+        int score = wins*3 + tie*1;
+        return score;
+    }
+    public int reset(SoccerTeam a){
+        int score = 0;
+        return score;
+    }
 }
